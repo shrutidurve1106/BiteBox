b0d635af947c21d6a7981225b05fc585aa25a0f4
shrutidurve1106 <shruti.durve@gmail.com>
2026-08-27 16:42:55 +0530

    changes updated

---
+++ Practice/script.js
@@ -0,0 +1,51 @@
+// document.write("Hello");
+
+// var a = 25;
+// console.log(a);
+// console.log(typeof a);
+
+// let name = prompt("Enter your name: ");
+// console.log(name);
+
+// let consider = confirm("Im giving exam.");
+// console.log(consider);
+
+// arithmetic operators
+// let a = 20;
+// let b = 10;
+// console.log("Addition: " + (a + b));
+// console.log("Subtraction: " + (a - b));
+// console.log("Multiplication: " + a * b);
+// console.log("Division: " + a / b);
+// console.log("Remainder: " + (a % b));
+
+// assignment Operators
+// let a = 25;
+// console.log(" = : ", a);
+// a += 5; // a = a+5
+// console.log(" += : ", a);
+// a -= 5;
+// console.log(" -= : ", a);
+// a *= 5;
+// console.log(" *= : ", a);
+// a /= 5;
+// console.log(" /= : ", a);
+// a %= 5;
+// console.log(" %= : ", a);
+
+// comparison operators
+// let p = 20;
+// let q = 21;
+// console.log("p:", p);
+// console.log("q:", q);
+// console.log("== :", p == q);
+// console.log("!= :", p != q);
+// console.log("> :", p > q);
+// console.log("< :", p < q);
+// console.log(">= :", p >= q);
+// console.log("<= :", p <= q);
+
+//logical operator
+// console.log(true && false);
+// console.log(true || false);
+// console.log(!true);
